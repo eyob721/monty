76391488a2633f09b176d5727ddfee9d95c650b3
eyob721 <eyobmelkamu721@gmail.com>
2023-09-20 12:50:39 +0300

    Rename the buffer macro

--- utils_general.c
+++ utils_general.c
@@ -14,24 +14,24 @@
  */
 int read_file(int fd, char **buf, int *size)
 {
-	char tmp_buf[BUF_SIZE] = {0};
-	int bytes_read = READ_EOF, buf_pos = 0;
+	char tmp_buf[FILE_BUF_SIZE] = {0};
+	int bytes_read, buf_pos = 0;
 
 	if (*buf == NULL)
 	{
-		*size = BUF_SIZE;
+		*size = FILE_BUF_SIZE;
 		*buf = _realloc(*buf, 0, *size);
 		if (*buf == NULL)
 			return (READ_ERR);
 	}
 
-	bytes_read = read(fd, tmp_buf, BUF_SIZE - 1);
+	bytes_read = read(fd, tmp_buf, FILE_BUF_SIZE - 1);
 	while (bytes_read != READ_EOF && bytes_read != READ_ERR)
 	{
 		if (bytes_read >= (*size - buf_pos))
 		{
-			*buf = _realloc(*buf, *size, *size + BUF_SIZE);
-			*size += BUF_SIZE;
+			*buf = _realloc(*buf, *size, *size + FILE_BUF_SIZE);
+			*size += FILE_BUF_SIZE;
 		}
 		_memcpy(*buf + buf_pos, tmp_buf, bytes_read);
 		buf_pos += bytes_read;
@@ -40,7 +40,7 @@ int read_file(int fd, char **buf, int *size)
 			(*buf)[buf_pos - 1] = '\0';
 			return (buf_pos);
 		}
-		bytes_read = read(STDIN_FILENO, tmp_buf, BUF_SIZE - 1);
+		bytes_read = read(fd, tmp_buf, FILE_BUF_SIZE - 1);
 	}
 	return (bytes_read);
 }
