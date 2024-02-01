ee24262df08389eed996c1c740c112d2bae29596
eyob721 <eyobmelkamu721@gmail.com>
2024-02-01 14:36:48 +0300

    feat: implement getting the correct opcode and opcode argument

--- monty_main.c
+++ monty_main.c
@@ -1,6 +1,8 @@
 #include "lib.h"
 #include "monty.h"
 
+monty_t monty = {NULL, NULL, EXIT_SUCCESS, NULL};
+
 /**
  * main - the monty program
  * @argc: argument count
@@ -28,10 +30,33 @@ int main(int argc, char **argv)
 	while (bytes_read != READ_EOF)
 	{
 		++line_number;
-		_printf("[%d]: %s\n", line_number, line_buf);
+		get_opcode_oparg(line_buf);
+		_printf("[%d]: opcode: [%s] oparg: [%s]\n", line_number,
+				monty.cur_opcode, monty.cur_oparg);
 		bytes_read = read_line(fd, &line_buf, &line_size);
 	}
 	close(fd);
 	free(line_buf);
 	return (EXIT_SUCCESS);
+}
+
+/**
+ * get_opcode_oparg - gets the opcode and oparg from the line buffer.
+ * @line_buf: pointer to the line buffer
+ *
+ * Return: void
+ */
+void get_opcode_oparg(char *line_buf)
+{
+	char *line_ptr = line_buf, *line_save = NULL;
+
+	if (line_buf == NULL)
+		return;
+
+	/* Skip initial spaces */
+	while (*line_ptr == ' ')
+		++line_ptr;
+
+	monty.cur_opcode = _strtok_r(line_ptr, " ", &line_save);
+	monty.cur_oparg = _strtok_r(NULL, " ", &line_save);
 }
