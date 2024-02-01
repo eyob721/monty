8b82c8a86ee267ff8c7b27d3e3f7c8f3babe913c
eyob721 <eyobmelkamu721@gmail.com>
2024-02-01 15:15:53 +0300

    feat: implement the push and pall opcodes

--- monty_main.c
+++ monty_main.c
@@ -1,4 +1,3 @@
-#include "lib.h"
 #include "monty.h"
 
 monty_t monty = {NULL, NULL, EXIT_SUCCESS, NULL};
@@ -12,8 +11,10 @@ monty_t monty = {NULL, NULL, EXIT_SUCCESS, NULL};
  */
 int main(int argc, char **argv)
 {
-	int line_size = 0, line_number = 0, fd, bytes_read;
+	int line_buf_size = 0, line_num = 0, fd, bytes_read;
 	char *line_buf = NULL;
+	stack_t *top = NULL;
+	void (*execute_opcode)(stack_t **top, unsigned int line_number);
 
 	if (argc != 2)
 	{
@@ -26,29 +27,40 @@ int main(int argc, char **argv)
 		_dprintf(STDERR_FILENO, "Error: Can't open file %s\n", argv[1]);
 		return (EXIT_FAILURE);
 	}
-	bytes_read = read_line(fd, &line_buf, &line_size);
+	bytes_read = read_line(fd, &line_buf, &line_buf_size);
 	while (bytes_read != READ_EOF)
 	{
-		++line_number;
+		++line_num;
 		get_opcode_oparg(line_buf);
-		_printf("[%d]: opcode: [%s] oparg: [%s]\n", line_number,
-				monty.cur_opcode, monty.cur_oparg);
-		bytes_read = read_line(fd, &line_buf, &line_size);
+		if (monty.cur_opcode != NULL) /* Here NULL means an empty line */
+		{
+			execute_opcode = get_opcode_function(monty.cur_opcode);
+			if (execute_opcode == NULL) /* Here NULL means an invalid opcode */
+			{
+				_dprintf(STDERR_FILENO, "L%d: unknown instruction %s\n",
+						 line_num, monty.cur_opcode);
+				monty.exit_status = EXIT_FAILURE;
+				break;
+			}
+			execute_opcode(&top, line_num);
+		}
+		bytes_read = read_line(fd, &line_buf, &line_buf_size);
 	}
 	close(fd);
 	free(line_buf);
-	return (EXIT_SUCCESS);
+	free_stack(top);
+	return (monty.exit_status);
 }
 
 /**
- * get_opcode_oparg - gets the opcode and oparg from the line buffer.
+ * get_opcode_oparg - gets the opcode and oparg from the line buffer
  * @line_buf: pointer to the line buffer
  *
  * Return: void
  */
 void get_opcode_oparg(char *line_buf)
 {
-	char *line_ptr = line_buf, *line_save = NULL;
+	char *line_ptr = line_buf, *line_save_ptr = NULL;
 
 	if (line_buf == NULL)
 		return;
@@ -57,6 +69,31 @@ void get_opcode_oparg(char *line_buf)
 	while (*line_ptr == ' ')
 		++line_ptr;
 
-	monty.cur_opcode = _strtok_r(line_ptr, " ", &line_save);
-	monty.cur_oparg = _strtok_r(NULL, " ", &line_save);
+	monty.cur_opcode = _strtok_r(line_ptr, " ", &line_save_ptr);
+	monty.cur_oparg = _strtok_r(NULL, " ", &line_save_ptr);
+}
+
+/**
+ * get_opcode_function - fetches the handler function for an opcode
+ * @opcode: given opcode
+ *
+ * Return: function of the opcode, or NULL if opcode is not supported
+ */
+void (*get_opcode_function(char *opcode))(stack_t **top, unsigned int line)
+{
+	instruction_t opcode_fun[] = {
+		{"push", push},
+		{"pall", pall},
+		{NULL,   NULL}
+	};
+	int i = 0;
+
+	while (opcode_fun[i].opcode != NULL)
+	{
+		if (_strcmp(opcode_fun[i].opcode, opcode) == 0)
+			return (opcode_fun[i].f);
+		++i;
+	}
+
+	return (NULL);
 }
---
+++ monty_opcode_handlers_1.c
@@ -0,0 +1,50 @@
+#include "monty.h"
+
+/**
+ * push - handles the push opcode
+ * @top: double pointer to the top of the stack
+ * @lnum: current line number in the file
+ *
+ * Return: void
+ */
+void push(stack_t **top, unsigned int lnum)
+{
+	stack_t *new_node = NULL;
+
+	/* Check if the opcode argument is an integer */
+	if (!is_integer(monty.cur_oparg))
+	{
+		_dprintf(STDERR_FILENO, "L%d: usage: push integer\n", lnum);
+		monty.exit_status = EXIT_FAILURE;
+		return;
+	}
+	/* Allocate memory */
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		_dprintf(STDERR_FILENO, "Error: malloc failed\n");
+		monty.exit_status = EXIT_FAILURE;
+		return;
+	}
+	/* Insert the new node into the stack */
+	new_node->n = _atoi(monty.cur_oparg);
+	push_to_top(top, new_node);
+	monty.exit_status = EXIT_SUCCESS;
+}
+
+/**
+ * pall - handles the pall opcode
+ * @top: double pointer to the top of the stack
+ * @lnum: current line number in the file
+ *
+ * Return: void
+ */
+void pall(stack_t **top, unsigned int lnum)
+{
+	stack_t *ptr;
+
+	(void)lnum;
+	for (ptr = *top; ptr != NULL; ptr = ptr->prev)
+		_printf("%d\n", ptr->n);
+	monty.exit_status = EXIT_SUCCESS;
+}
